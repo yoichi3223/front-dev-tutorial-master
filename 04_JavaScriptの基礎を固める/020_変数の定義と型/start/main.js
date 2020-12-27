16ffffbbf51a873793a7aa2cf8dc3523b6d2a0ce
yoichihashimoto <yoichi3223@gmail.com>
2020-12-27 14:47:10 +0900

    js add

---
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,12 @@
+// window.alert(5 + 5);
+
+// let name = "洋一";
+// name = 12;
+// console.log(typeof name);
+
+const hello = function(name,age){
+  // let name = 
+console.log(name + age);
+}
+
+ hello("洋一",29);

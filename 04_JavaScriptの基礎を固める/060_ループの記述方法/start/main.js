92646a91eaedd071ebda750451fd406a9ddee754
yoichihashimoto <yoichi3223@gmail.com>
2020-12-29 17:08:58 +0900

    allowfunction add

---
+++ 04_JavaScriptの基礎を固める/060_ループの記述方法/start/main.js
@@ -0,0 +1,6 @@
+const arry = [1,2,3,4,5,6];
+
+for(let z of arry){
+
+  console.log(z);
+}
---
+++ 04_JavaScriptの基礎を固める/062_ループの記述方法（実践）/start/main.js
@@ -0,0 +1,42 @@
+// let todos = [
+// {
+//   id:1,
+//   title:"Go to school",
+//   completed:true
+// },
+// {
+//   id:2,
+//   title:"Go to museum",
+//   completed:true
+// },
+// {
+//   id:3,
+//   title:"Go to shopping",
+//   completed:false
+// }
+
+
+// ]
+
+// for(let todo of todos){
+//   // let todo = todos[i];
+//   if(todo.completed === true){
+//       console.log(todo.title);
+// }
+//   }
+
+// const hello = name =>{
+//   console.log('hello' + name);
+// }
+
+// hello("洋一");
+
+// const animal = name=> console.log(name);
+
+
+// animal("ライオン");
+
+
+const names = ["洋一","洋一","洋一","洋一","洋一","洋一","洋一","洋一","洋一","洋一","洋一","洋一","洋一","洋一"]
+console.log(names);
+names.forEach(value => console.log(value + "さん"));

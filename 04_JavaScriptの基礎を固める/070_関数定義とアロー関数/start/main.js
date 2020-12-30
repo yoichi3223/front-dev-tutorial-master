2b864db3803ef8b924cacfc64d71230e954b03a7
yoichihashimoto <yoichi3223@gmail.com>
2020-12-30 10:14:45 +0900

    object practice

---
+++ 04_JavaScriptの基礎を固める/070_関数定義とアロー関数/start/main.js
@@ -0,0 +1,21 @@
+const names = [
+{
+  name:"洋一1",
+  age:29,
+  born:"福島"
+},
+{
+  name:"洋一2",
+  age:29,
+  born:"福島"
+}
+,
+{
+  name:"洋一3",
+  age:29,
+  born:"福島"
+}
+
+]
+
+window.alert(names[0].name);

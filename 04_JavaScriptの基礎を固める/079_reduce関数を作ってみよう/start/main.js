314e1722301afedee612cd297311feee13081770
yoichihashimoto <yoichi3223@gmail.com>
2021-01-02 14:37:18 +0900

    reduce method

--- 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
+++ 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
@@ -5,5 +5,14 @@ function tag(accu, curr) {
     return `${accu}<${curr}>`;
 }
 
-const result = strArry.reduce(tag, "");
+function reduce(arry,callback,defaultValue){
+    let accu = defaultValue;
+for(let i = 0;i < arry.length; i++){
+   let curr = arry[i];
+   accu = callback(accu,curr);
+}
+return accu;
+}
+
+const result = reduce(strArry,tag, "");
 console.log(result);

fc72e7f8824c0b45c8f9af0368ad891bbd29707b
yoichihashimoto <yoichi3223@gmail.com>
2021-01-05 00:16:09 +0900

    function-practice add

--- 04_JavaScriptの基礎を固める/110_DOM操作練習（文字分割）/start/main.js
+++ 04_JavaScriptの基礎を固める/110_DOM操作練習（文字分割）/start/main.js
@@ -1,12 +1,34 @@
-document.addEventListener("DOMContentLoaded",function(){
+// document.addEventListener("DOMContentLoaded",function(){
 
-const el = document.querySelector(".animate-title");
-const str = el.innerHTML.trim();
-let concaSta = "";
-for(let c of str){
-  concaSta += `<span class = "char">${c}</span>`;
+// const el = document.querySelector(".animate-title");
+// const str = el.innerHTML.trim();
+// let concaSta = "";
+// for(let c of str){
+//   concaSta += `<span class = "char">${c}</span>`;
+// }
+
+// el.innerHTML = concaSta;
+// });
+
+function food(){
+  console.log("新しい仕事の始まり。")
+  console.log("カレーを食べてがんばりましょう")
 }
 
-el.innerHTML = concaSta;
-});
+function newyear(){
+  console.log("明けましておめでとうございます。")
+  food("カレー");
+}
 
+
+const company = function(callback,name,age){
+console.log(`${name}です。${age}歳です`)
+callback();
+
+
+}
+const start = function(){
+company(newyear,"橋本洋一",29)
+}
+
+start();
